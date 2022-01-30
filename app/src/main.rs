81728188c6a9432ca997fffdf86290fb00792251
Derrick Plotsky <derrickplotsky@gmail.com>
2022-01-30 13:11:43 -0800

    Add yew app
    This is a super basic frontend app that lets someone generate a room
    and description of inhabitants.

---
+++ app/src/main.rs
@@ -0,0 +1,55 @@
+use underworld_core::generators::{rooms::RoomPrototype, generator::Generator};
+use yew::prelude::*;
+
+
+enum RoomMsg {
+    GenerateRoom,
+}
+
+struct RoomDescriptions {
+    room_description: String,
+    inhabitants_description: String,
+}
+
+impl Component for RoomDescriptions {
+    type Message = RoomMsg;
+
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self {
+            room_description: "".to_string(),
+            inhabitants_description: "".to_string(),
+        }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            RoomMsg::GenerateRoom => {
+                let room_prototype = RoomPrototype::build_random(Vec::new(), 1..4);
+                let room = room_prototype.generate();
+                self.room_description = format!("{}", &room);
+                self.inhabitants_description = room.look_at_inhabitants();
+                // the value has changed so we need to
+                // re-render for it to appear on the page
+                true
+            }
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        // This gives us a component's "`Scope`" which allows us to send messages, etc to the component.
+        let link = ctx.link();
+        html! {
+            <div>
+                <button onclick={link.callback(|_| RoomMsg::GenerateRoom)}>{ "Generate" }</button>
+                <p>{ &self.room_description }</p>
+                <p>{ &self.inhabitants_description }</p>
+            </div>
+        }
+    }
+}
+
+fn main() {
+    yew::start_app::<RoomDescriptions>();
+}
