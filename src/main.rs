9d02ea776148ce53a871728dbe2e09a725e5f833
Derrick Plotsky <derrickplotsky@gmail.com>
2022-05-26 20:18:19 -0700

    Bump API version

--- src/main.rs
+++ src/main.rs
@@ -46,7 +46,7 @@ async fn main() -> Result<(), std::io::Error> {
             UnderworldGameActionApi,
         ),
         "Underworld",
-        "0.1.0",
+        "0.2.0",
     )
     .server(get_server_url());
 
