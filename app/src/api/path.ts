ffca872b11a664f0703adebd7a35e68d37a8b2db
Derrick Plotsky <derrickplotsky@gmail.com>
2022-05-31 22:22:02 -0700

    Add debug url

--- app/src/api/path.ts
+++ app/src/api/path.ts
@@ -1,4 +1,5 @@
 export const getBasePath = (): string => {
+  console.log(window.location.href);
   if (window.location.href === "http://localhost:3000/") {
     return "http://localhost:8080/api";
   }
