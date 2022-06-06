1155fcc673a70433fff55b084690720fdc1e647b
Derrick Plotsky <derrickplotsky@gmail.com>
2022-06-05 19:05:29 -0700

    Some small updates to the UI

--- src/components/RoomView/RoomView.tsx
+++ src/components/RoomView/RoomView.tsx
@@ -1,5 +1,10 @@
 import { FunctionComponent } from "react";
-import { ExitRoom, PerformAction, Room, RoomType } from "../../generated-api";
+import {
+  ExitRoom,
+  FlavourText,
+  PerformAction,
+  Room,
+} from "../../generated-api";
 import { ExitView } from "../ExitView/ExitView";
 
 import styles from "./styles.module.css";
@@ -9,63 +14,73 @@ export interface RoomViewProps {
   actions: Array<PerformAction>;
 }
 
-const typeText = (roomType: RoomType): string => {
-  switch (roomType) {
-    case "cave":
-      return "Cave";
-    case "cavern":
-      return "Cavern";
-    case "cemetery":
-      return "Cemetery";
-    case "crypt":
-      return "Crypt";
-    case "entry_way":
-      return "Entry way";
-    case "mausoleum":
-      return "Mausoleum";
-    case "prison_cell":
-      return "Prison cell";
-    case "room":
-      return "Room";
-    case "tavern_hall":
-      return "Tavern hall";
-    case "temple_hall":
-      return "Temple hall";
+const flavourText = (flavour: FlavourText): string => {
+  switch (flavour) {
+    case "a_strange_breeze_blows":
+      return "A strange breeze blows through the room.";
+    case "mold_moss_covers_walls":
+      return "A strange mold and moss cover the walls.";
+    case "unseen_lights_flicker_walls":
+      return "Unseen lights flicker across the walls.";
   }
+};
+
+const description = (room: Room): string => {
+  const sizes: Array<string> = [];
+
+  if (room.dimensions.height !== "average") {
+    sizes.push(room.dimensions.height.replaceAll("_", ""));
+  }
+
+  if (room.dimensions.length !== "average") {
+    sizes.push(room.dimensions.length.replaceAll("_", " "));
+  }
+
+  if (room.dimensions.width !== "average") {
+    sizes.push(room.dimensions.width.replaceAll("_", " "));
+  }
+
+  const descriptors = room.descriptors
+    .slice()
+    .sort((a, b) => a.localeCompare(b))
+    .map((t) => t.replaceAll("_", " "));
+
+  const flavour: Array<string> = [];
+  if (room.flavour) {
+    flavour.push(flavourText(room.flavour));
+  }
+
+  return [
+    ...sizes,
+    ...descriptors,
+    `${room.room_type.replaceAll("_", " ")}.`,
+    ...flavour,
+  ].join(" ");
 };
 
 export const RoomView: FunctionComponent<RoomViewProps> = ({
   room,
   actions,
-}) => {
-  const description = [
-    ...room.descriptors.sort((a, b) => a.localeCompare(b)),
-    typeText(room.room_type),
-  ].join(" ");
+}) => (
+  <div className={styles.room}>
+    <span className={styles.description}>{`You are in a ${description(
+      room,
+    )}`}</span>
+    {room.exits.length > 0 && (
+      <div className={styles.exits}>
+        <span
+          className={styles["exit-title"]}
+        >{`You see ${room.exits.length} exits you can jump through.`}</span>
+        {room.exits.map((exit) => {
+          const exitAction = actions.find(
+            (action) =>
+              action.name === "exit_room" &&
+              (action.args! as ExitRoom).exit_id === exit.id,
+          );
 
-  return (
-    <div className={styles.room}>
-      <span
-        className={styles.description}
-      >{`You are in a ${description}`}</span>
-      {room.exits.length > 0 && (
-        <div className={styles.exits}>
-          <span
-            className={styles["exit-title"]}
-          >{`You see ${room.exits.length} exits you can jump through.`}</span>
-          {room.exits.map((exit) => {
-            const exitAction = actions.find(
-              (action) =>
-                action.name === "exit_room" &&
-                (action.args! as ExitRoom).exit_id === exit.id,
-            );
-
-            return (
-              <ExitView key={exit.id} exit={exit} exitAction={exitAction} />
-            );
-          })}
-        </div>
-      )}
-    </div>
-  );
-};
+          return <ExitView key={exit.id} exit={exit} exitAction={exitAction} />;
+        })}
+      </div>
+    )}
+  </div>
+);
