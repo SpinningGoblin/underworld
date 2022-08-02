4c8bfa5ef44f2391863a34b4bc24c3d94804cc24
Derrick Plotsky <derrickplotsky@gmail.com>
2022-08-02 00:15:56 -0700

    Bootstrap our auth services
    Also ensure that the UI can only be accessed if you are logged in.

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,38 @@
 mod actions;
 mod api;
+mod auth;
+pub mod config;
 mod error;
 mod event;
 mod game;
 mod player_characters;
-mod psql;
 pub mod tags;
 
-use std::env;
-
 use api::{
-    game::UnderworldGameApi, game_actions::UnderworldGameActionApi, npc::UnderworldNpcApi,
-    player::UnderworldPlayerApi,
+    auth::UnderworldAuthApi, game::UnderworldGameApi, game_actions::UnderworldGameActionApi,
+    middleware::CookieToTokenMiddleware, npc::UnderworldNpcApi, player::UnderworldPlayerApi,
 };
+use config::{get_port, get_psql_url, get_server_api_url, get_server_auth_url};
 use poem::{
-    endpoint::StaticFilesEndpoint, listener::TcpListener, middleware::Cors, EndpointExt, Result,
-    Route, Server,
+    endpoint::StaticFilesEndpoint,
+    http::StatusCode,
+    listener::TcpListener,
+    middleware::Cors,
+    session::{CookieConfig, CookieSession},
+    EndpointExt, IntoResponse, Result, Route, Server,
 };
 use poem_openapi::OpenApiService;
-use psql::get_psql_url;
+use sqlx::PgPool;
 
-fn get_port() -> u16 {
-    env::var("PORT")
-        .ok()
-        .and_then(|p| p.parse().ok())
-        .unwrap_or(8080)
-}
+struct UnauthResponse;
 
-fn get_server_url() -> String {
-    let base_url = env::var("SERVER_URL")
-        .ok()
-        .unwrap_or(format!("http://localhost:{}", get_port()));
-    format!("{}/api", base_url)
+impl IntoResponse for UnauthResponse {
+    fn into_response(self) -> poem::Response {
+        poem::Response::builder()
+            .status(StatusCode::FOUND)
+            .header("Location", "/")
+            .finish()
+    }
 }
 
 #[tokio::main]
@@ -48,7 +49,10 @@ async fn main() -> Result<(), std::io::Error> {
         "Underworld",
         "0.5.0",
     )
-    .server(get_server_url());
+    .server(get_server_api_url());
+
+    let auth_service = OpenApiService::new(UnderworldAuthApi, "UnderworldAuth", "0.5.0")
+        .server(format!("{}/auth", get_server_auth_url()));
 
     let ui = api_service.swagger_ui();
     let spec = api_service.spec();
@@ -59,12 +63,64 @@ async fn main() -> Result<(), std::io::Error> {
         )
         .nest(
             "/",
-            StaticFilesEndpoint::new("./public").index_file("index.html"),
+            StaticFilesEndpoint::new("./public")
+                .index_file("index.html")
+                .before(|req| async move {
+                    let token_cookie = req.cookie().get("underworldToken");
+                    let token: String = match &token_cookie {
+                        Some(token_cookie) => token_cookie.value_str().to_string(),
+                        None => {
+                            return Err(poem::Error::from_status(StatusCode::UNAUTHORIZED));
+                        }
+                    };
+
+                    let pool_option: Option<&PgPool> = req.data();
+                    let pool = pool_option.unwrap();
+
+                    let valid_token = auth::repository::valid_api_token(pool, &token).await;
+                    match valid_token {
+                        Ok(_) => {}
+                        Err(_) => {
+                            return Err(poem::Error::from_status(StatusCode::UNAUTHORIZED));
+                        }
+                    }
+
+                    Ok(req)
+                })
+                .after(|result| async move {
+                    match result {
+                        Err(e) => {
+                            let response = e.into_response();
+                            match &response.status() {
+                                &StatusCode::UNAUTHORIZED => Ok(poem::Response::builder()
+                                    .header("Location", "/sign-in")
+                                    .status(StatusCode::FOUND)
+                                    .finish()),
+                                _ => Ok(response),
+                            }
+                        }
+                        Ok(response) => Ok(response),
+                    }
+                }),
         )
-        .nest("/api", api_service)
+        .nest(
+            "/sign-in",
+            StaticFilesEndpoint::new("./sign-in").index_file("index.html"),
+        )
+        .nest(
+            "/success",
+            StaticFilesEndpoint::new("./success").index_file("index.html"),
+        )
+        .nest(
+            "/token",
+            StaticFilesEndpoint::new("./token").index_file("index.html"),
+        )
+        .nest("/api", api_service.with(CookieToTokenMiddleware))
+        .nest("/auth", auth_service)
         .nest("/swagger_ui", ui)
         .at("/spec", poem::endpoint::make_sync(move |_| spec.clone()))
         .with(Cors::new())
+        .with(CookieSession::new(CookieConfig::default().secure(false)))
         .data(pool);
 
     let listen_url = format!("0.0.0.0:{}", get_port());
--- src/psql.rs
+++
@@ -1,4 +0,0 @@
-use std::env;
-pub fn get_psql_url() -> String {
-    env::var("DATABASE_URL").unwrap()
-}
