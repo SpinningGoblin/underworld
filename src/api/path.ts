e75ae254209f75c4f03cc4b653ff636cc0044384
Derrick Plotsky <derrickplotsky@gmail.com>
2022-08-03 23:27:06 -0700

    Update sign in url

--- src/api/path.ts
+++ src/api/path.ts
@@ -1,7 +1,7 @@
 const backendUrl = import.meta.env.VITE_UNDERWORLD_URL;
 
 export const getBasePath = (): string => `${backendUrl}/api`;
-export const getSignInUrl = () => `${backendUrl}/sign-in`;
+export const getSignInUrl = () => `${window.location.origin}/sign-in`;
 export const getApiLoginUrl = () => `${backendUrl}/auth/login`;
 export const getMailCallbackUrl = (mailToken: string) =>
   `${backendUrl}/auth/enter_the_underworld?token=${mailToken}`;
