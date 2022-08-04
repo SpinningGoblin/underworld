609b15ca94d695c1d3c73c2fe403cdf0b1ef4036
Derrick Plotsky <derrickplotsky@gmail.com>
2022-08-04 12:02:02 -0700

    Setup react-query

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { setAuthToken } from "./api/configuration";
-import { getApiToken } from "./api/tokens";
+import { getApiToken, removeApiToken } from "./api/tokens";
 import { App } from "./App";
 import { ThemeWrapper } from "./themes/ThemeWrapper";
 
@@ -11,6 +11,8 @@ import "./index.css";
 import { SignInScreen } from "./screens/SignIn";
 import { EmailSuccessScreen } from "./screens/EmailSuccess";
 import { getMailCallbackUrl, getSignInUrl } from "./api/path";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ResponseError } from "./generated-api";
 
 (() => {
   const params = new URL(window.location.href).searchParams;
@@ -32,17 +34,35 @@ import { getMailCallbackUrl, getSignInUrl } from "./api/path";
     return;
   }
 
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        retry: false,
+        cacheTime: Infinity,
+        onError: (err) => {
+          if (err instanceof ResponseError && err.response.status === 401) {
+            removeApiToken();
+            window.location.assign(getSignInUrl());
+          }
+        },
+      },
+    },
+  });
+
   ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-      <BrowserRouter>
-        <ThemeWrapper>
-          <Routes>
-            <Route path="/" element={<App />}></Route>
-            <Route path="/sign-in" element={<SignInScreen />}></Route>
-            <Route path="/success" element={<EmailSuccessScreen />}></Route>
-          </Routes>
-        </ThemeWrapper>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <ThemeWrapper>
+            <Routes>
+              <Route path="/" element={<App />}></Route>
+              <Route path="/sign-in" element={<SignInScreen />}></Route>
+              <Route path="/success" element={<EmailSuccessScreen />}></Route>
+            </Routes>
+          </ThemeWrapper>
+        </BrowserRouter>
+      </QueryClientProvider>
     </React.StrictMode>,
   );
 })();
