49c8cdfba93234b42e3a5003aab0ad4f23da050f
Derrick Plotsky <derrickplotsky@gmail.com>
2022-08-04 19:58:44 -0700

    Text content is proper colour

--- src/screens/Loading/LoadingScreen.tsx
+++ src/screens/Loading/LoadingScreen.tsx
@@ -1,11 +1,16 @@
 import { FunctionComponent } from "react";
+import { useTheme } from "../../themes";
 
 import styles from "./LoadingScreen.module.css";
 
-export const LoadingScreen: FunctionComponent = () => (
-  <div className={styles.body}>
-    <div className={styles.container}>
-      <h1 className={styles.loading}>Loading...</h1>
+export const LoadingScreen: FunctionComponent = () => {
+  const { theme } = useTheme();
+
+  return (
+    <div className={styles.body} style={{ color: theme.colors.secondary }}>
+      <div className={styles.container}>
+        <h1 className={styles.loading}>Loading...</h1>
+      </div>
     </div>
-  </div>
-);
+  );
+};
